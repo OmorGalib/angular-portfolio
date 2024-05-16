957ac1beeb88e1f72400c357c219096814248a5c
Omor Galib <omorgalib007@gmail.com>
2024-05-16 15:54:37 +0600

    lol

--- src/app/pages/home/cards-section/cards-section.component.ts
+++ src/app/pages/home/cards-section/cards-section.component.ts
@@ -8,4 +8,13 @@ import { CARD } from 'src/app/core/db/card.db';
 })
 export class CardsSectionComponent {
   i: any[] = CARD ;
+  imageUrls: string[] = [];
+  imageUrls1: string[] = [];
+
+  constructor() {
+    for (let i = 0; i < 100; i++) {
+      this.imageUrls.push("../../../../assets/images/img/img.jpg");
+      this.imageUrls1.push("../../../../assets/images/img/download.jpg");
+    }
+  }
 }
